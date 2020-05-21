6fa4644db94a057fd025f9dd8d32f953ea7e316e
Michael Wisnewski <mcwtennis@gmail.com>
2020-05-20 19:41:34 -0500

    test dependencies file

---
+++ kerasTest.py
@@ -0,0 +1,11 @@
+# Keras Test
+
+import numpy
+import cv2
+import tensorflow as tf
+from tensorflow import keras
+import keyboard
+
+print('Tensorflow Version: %s' % tf.__version__)
+print('OpenCV2 Version: %s' % cv2.__version__)
+print('Keras Version: %s' % keras.__version__)
